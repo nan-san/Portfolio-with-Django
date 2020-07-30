62c665219d8d76a7bd182cb734b9bc2a16e3ecf8
minamiatsuko <minmin-373@hotmail.co.jp>
2020-07-30 23:54:29 +0200

    お問合せフォーム実装＆アニメーションをhomt.htmlに移動

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path,include
 from . import views
+from .views import ContactFormView, ContactResultView
 
 
 app_name = 'portfolio'
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
-   
+    # path('', views.HomeView.as_view(), name='home'),
+    path('', views.home, name='home'),
     
-]
+    path('contact/', ContactFormView.as_view(), name='contact_form'),
+    path('contact/result/', ContactResultView.as_view(), name='contact_result'),
 
+]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
-
-class HomeView(TemplateView):
-    template_name = "portfolio/home.html"
-    
+from django.views.generic.edit import FormView
+from .forms import ContactForm
 
 
+# from django.views.generic import TemplateView
+
+# class HomeView(TemplateView):
+#     template_name = "portfolio/home.html"
+
+
+def home(request):
+    return render(request,'portfolio/home.html')
+
+
+class ContactFormView(FormView):
+    template_name = 'contact/contact_form.html'
+    form_class = ContactForm
+    success_url = reverse_lazy('portfolio:contact_result')
+
+    def form_valid(self, form):
+        form.send_email()
+        return super().form_valid(form)
+
+
+class ContactResultView(TemplateView):
+    template_name = 'contact/contact_result.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['success'] = "お問い合わせは正常に送信されました。"
+        return context
