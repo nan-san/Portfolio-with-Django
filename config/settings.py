6714e417e6ffa961985cc592e1f65bc840520401
minamiatsuko <minmin-373@hotmail.co.jp>
2020-08-11 18:04:27 +0200

    pythonAnywhereへのセッティング

--- config/settings.py
+++ config/settings.py
@@ -131,3 +131,8 @@ EMAIL_HOST_USER = 'kitztzki@gmail.com'
 EMAIL_HOST_PASSWORD = 'cxqldrfilkpkqifj'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+
+# Pythonanywhere用の設定を追加
+
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
--- config/wsgi.py
+++ config/wsgi.py
@@ -8,10 +8,15 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+path = '/home/NanWeb/portfolio'
+if path not in sys.path:
+    sys.path.append(path)
 
+os.environ['DJANGO_SETTINGS_MODULE']= 'config.settings'
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
